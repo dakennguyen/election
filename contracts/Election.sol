7e3344dba59b778d1d8c84cd8b78cb7e0886e96d
Khoa Nguyen <ngdoan.anhkhoa@gmail.com>
2019-11-03 15:22:02 +0700

    Cast votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,9 @@ contract Election {
         uint voteCount;
     }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
     // Store Candidates
     // Fetch Candidates
     mapping(uint => Candidate) public candidates;
@@ -20,8 +23,22 @@ contract Election {
         addCandidate("Candidate 2");
     }
 
-    function addCandidate (string memory name) private {
+    function addCandidate(string memory name) private {
         candidatesCount++;
         candidates[candidatesCount] = Candidate(candidatesCount, name, 0);
+    }
+
+    function vote(uint candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(candidateId > 0 && candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[candidateId].voteCount++;
     }
 }
